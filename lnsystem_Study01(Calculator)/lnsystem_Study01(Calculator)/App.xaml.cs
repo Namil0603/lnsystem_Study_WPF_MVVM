dbc2364f977ecf5b2cefbe2f126e86b829030285
HyunSooKim <qqa0603@naver.com>
2024-08-19 11:24:03 +0900

    1Week_Calculator_InitialCommit

---
+++ lnsystem_Study01(Calculator)/lnsystem_Study01(Calculator)/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace lnsystem_Study01_Calculator_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
