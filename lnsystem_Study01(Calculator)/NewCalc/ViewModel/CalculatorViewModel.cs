62395b09ba83902ebbad811eb5e773bbe7996846
HyunSooKim <qqa0603@naver.com>
2024-08-20 14:55:52 +0900

    SubProject Add

---
+++ lnsystem_Study01(Calculator)/NewCalc/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,101 @@
+﻿using NewCalc.Model;
+using NewCalc.Tool;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace NewCalc.ViewModel
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        // Commands
+        public ICommand CalculateCommand { get; }
+
+        // Properties
+        public ObservableCollection<string> Operators { get; }
+
+        private string firstInput;
+        private string secondInput;
+        private string finalText;
+        private string selectedOperators;
+
+        public string FirstInput
+        {
+            get => firstInput;
+            set => SetField(ref firstInput, value);
+        }
+
+        public string SecondInput
+        {
+            get => secondInput;
+            set => SetField(ref secondInput, value);
+        }
+
+        public string FinalText
+        {
+            get => finalText;
+            set => SetField(ref finalText, value);
+        }
+        public string SelectedOperators
+        {
+            get => selectedOperators;
+            set
+            {
+                selectedOperators = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private readonly CalculatorModel calculatorModel;
+
+        // Constructor
+        public CalculatorViewModel()
+        {
+            CalculateCommand = new RelayCommand(CalculateExecute);
+            Operators = new ObservableCollection<string> { "+", "-", "*", "/" };
+            calculatorModel = new CalculatorModel();
+        }
+
+        // Methods
+        private void CalculateExecute()
+        {
+            if (string.IsNullOrEmpty(FirstInput) || string.IsNullOrEmpty(SecondInput) || string.IsNullOrEmpty(SelectedOperators))
+            {
+                FinalText = "Please fill all the fields";
+                return;
+            }
+
+            if (!double.TryParse(FirstInput, out double firstNumber) || !double.TryParse(SecondInput, out double secondNumber))
+            {
+                FinalText = "Please enter a valid number";
+                return;
+            }
+
+            FinalText = SelectedOperators switch
+            {
+                "+" => calculatorModel.Add(firstNumber, secondNumber).ToString(),
+                "-" => calculatorModel.Subtract(firstNumber, secondNumber).ToString(),
+                "*" => calculatorModel.Multiply(firstNumber, secondNumber).ToString(),
+                "/" => calculatorModel.Divide(firstNumber, secondNumber).ToString(),
+                _ => "Invalid Operator"
+            };
+        }
+
+        // INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ lnsystem_Study01(Calculator)/NewNewCalc/ViewModel/VMCalculatorView.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using NewNewCalc.Tool;
+
+namespace NewNewCalc.ViewModel
+{
+    public class VMCalculatorView : INotifyPropertyChanged
+    {
+        public ICommand Input0 { get; }
+        public ICommand Input1 { get; }
+        public ICommand Input2 { get; }
+        public ICommand Input3 { get; }
+        public ICommand Input4 { get; }
+        public ICommand Input5 { get; }
+        public ICommand Input6 { get; }
+        public ICommand Input7 { get; }
+        public ICommand Input8 { get; }
+        public ICommand Input9 { get; }
+        public ICommand InputAdd { get; }
+        public ICommand InputSubtract { get; }
+        public ICommand InputMultiply { get; }
+        public ICommand InputDivide { get; }
+        public ICommand InputRemove { get; }
+        public ICommand InputResult { get; }
+        public ICommand InputClear { get; }
+        public ICommand InputDecimal { get; }
+
+        private string _currentInputField;
+        private string _lastiesInput;
+        private string _currentOperator;
+
+        public string CurrentInputField
+        {
+            get => _currentInputField;
+            set => SetField(ref _currentInputField, value);
+        }
+        public string LastiesInput
+        {
+            get => _lastiesInput;
+            set => SetField(ref _lastiesInput, value);
+        }
+
+        public VMCalculatorView()
+        {
+            Input0 = new RelayCommand(() => CurrentInputField += "0");
+            Input1 = new RelayCommand(() => CurrentInputField += "1");
+            Input2 = new RelayCommand(() => CurrentInputField += "2");
+            Input3 = new RelayCommand(() => CurrentInputField += "3");
+            Input4 = new RelayCommand(() => CurrentInputField += "4");
+            Input5 = new RelayCommand(() => CurrentInputField += "5");
+            Input6 = new RelayCommand(() => CurrentInputField += "6");
+            Input7 = new RelayCommand(() => CurrentInputField += "7");
+            Input8 = new RelayCommand(() => CurrentInputField += "8");
+            Input9 = new RelayCommand(() => CurrentInputField += "9");
+            InputClear = new RelayCommand(Clear);
+            InputRemove = new RelayCommand(RemoveLastCharacter);
+            InputAdd = new RelayCommand(() => SetOperator("+"));
+            InputSubtract = new RelayCommand(() => SetOperator("-"));
+            InputMultiply = new RelayCommand(() => SetOperator("*"));
+            InputDivide = new RelayCommand(() => SetOperator("/"));
+            InputResult = new RelayCommand(CalculateResult);
+            InputDecimal = new RelayCommand(AddDecimalPoint);
+        }
+
+        public VMCalculatorView(ICommand input6)
+        {
+            Input6 = input6;
+        }
+
+        private void Clear()
+        {
+            CurrentInputField = "";
+            LastiesInput = "";
+            _currentOperator = null;
+        }
+
+        private void RemoveLastCharacter()
+        {
+            if (!string.IsNullOrEmpty(CurrentInputField))
+            {
+                CurrentInputField = CurrentInputField.Remove(CurrentInputField.Length - 1);
+            }
+        }
+
+        private void SetOperator(string operatorSymbol)
+        {
+            if (!string.IsNullOrEmpty(CurrentInputField))
+            {
+                LastiesInput = CurrentInputField;
+                CurrentInputField = "";
+                _currentOperator = operatorSymbol;
+            }
+        }
+
+        private void CalculateResult()
+        {
+            if (!string.IsNullOrEmpty(CurrentInputField) && !string.IsNullOrEmpty(LastiesInput) && !string.IsNullOrEmpty(_currentOperator))
+            {
+                double num1 = double.Parse(LastiesInput);
+                double num2 = double.Parse(CurrentInputField);
+                double result = 0;
+
+                switch (_currentOperator)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        if (num2 != 0)
+                        {
+                            result = num1 / num2;
+                        }
+                        else
+                        {
+                            // Handle division by zero if necessary
+                        }
+                        break;
+                }
+
+                CurrentInputField = result.ToString();
+                LastiesInput = "";
+                _currentOperator = null;
+            }
+        }
+
+        private void AddDecimalPoint()
+        {
+            if (!CurrentInputField.Contains("."))
+            {
+                CurrentInputField += ".";
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
