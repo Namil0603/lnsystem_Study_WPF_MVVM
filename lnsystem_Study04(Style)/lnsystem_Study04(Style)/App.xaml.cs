4d4e587bf64e149a01c2cd3429857b7cd851dd12
HyunSooKim <qqa0603@naver.com>
2024-09-09 16:22:50 +0900

    4주차 빈 프로젝트 추가

---
+++ lnsystem_Study04(Style)/lnsystem_Study04(Style)/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace lnsystem_Study04_Style_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
